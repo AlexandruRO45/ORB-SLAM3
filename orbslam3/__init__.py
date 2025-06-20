1d46cbacf0a15bc649ec57894deb491fb1bd44ff
AlexandruRO45 <savaalexandru562@gmail.com>
2025-06-20 15:55:52 +0300

    v1.2.5 - Improvements in building the overall project

--- orbslam3/__init__.py
+++ orbslam3/__init__.py
@@ -1,1 +1,22 @@
-from ._internal import *
+"""
+ORB-SLAM3 Python bindings
+
+This package provides Python bindings for the ORB-SLAM3 visual SLAM system.
+"""
+
+__version__ = "1.2.5"
+__author__ = "Alex S."
+__email__ = "savaalexandru562@gmail.com"
+
+try:
+    from .orbslam3 import *  # Import the compiled extension (matches CMake target)
+except ImportError as e:
+    raise ImportError(
+        "Failed to import ORB-SLAM3 core module. "
+        "Make sure the package was installed correctly."
+    ) from e
+
+# You can add convenience functions here
+def get_version():
+    """Return the version of the orbslam3 package."""
+    return __version__
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import tarfile
 import numpy as np
 # Make sure to import find_packages
 from setuptools import Extension, setup, find_packages
@@ -22,11 +23,21 @@ class CMakeBuild(build_ext):
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+            
+        # Extract ORBvoc.txt.tar.gz before CMake (or after, if CMake moves it)
+        vocab_src = os.path.join(ext.sourcedir, "third_party", "ORB_SLAM3_engine", "Vocabulary", "ORBvoc.txt.tar.gz")
+        vocab_dst_dir = os.path.join(ext.sourcedir, "orbslam3")
+
+        if os.path.exists(vocab_src):
+            with tarfile.open(vocab_src, "r:gz") as tar:
+                tar.extractall(path=vocab_dst_dir)
+                print(f"Extracted ORBvoc.txt to {vocab_dst_dir}")
 
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE=Release",
+            f"-DCMAKE_CXX_FLAGS='-w'",
             f"-DCMAKE_CXX_FLAGS='-I {np.get_include()}'"
         ]
 
@@ -38,15 +49,21 @@ class CMakeBuild(build_ext):
 
 setup(
     name="orbslam3",
-    version="1.2.1",
+    version="1.2.5",
     description='SLAM and Global VO module for VNAV project',
     long_description="This package provides Python bindings for the ORB-SLAM3 visual SLAM system, allowing users to integrate SLAM functionalities into Python applications.",
     maintainer='Alex S.',
-    maintainer_email='alexandru.sava@interactive.ro',
+    maintainer_email='savaalexandru562@gmail.com',
     license='TODO: License declaration',
+    url='https://github.com/AlexandruRO45/ORB-SLAM3',
+    packages=find_packages(),
+    package_dir={'': '.'},
+    package_data={
+        'orbslam3': ['*.so', '*.dll', '*.dylib'],  # Include shared libraries
+    },
+    include_package_data=True,
     install_requires=["numpy"],
-    ext_modules=[CMakeExtension("orbslam3._internal")],
-    packages=["orbslam3"],
+    ext_modules=[CMakeExtension("orbslam3.orbslam3")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
